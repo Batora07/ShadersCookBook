0eb9a29730ce2ad9bb79ef6b449c22d85f32eff1
Batora <batora.ushiromiya.07@gmail.com>
2018-09-01 15:45:21 +0200

    Add new shaders, end of chapter 1 and 2
    new shaders : Holographic / Rim Effect, Textures Blending, Radius
    (useful for target focus)
    + add package

---
+++ Scripts/SetRadiusProperties.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SetRadiusProperties : MonoBehaviour {
+
+    public bool isEnemy = false;
+    public bool isEnabled = true;
+    public Material radiusMaterial;
+    public float radius = 1;
+    public Color color = Color.white;
+	
+	void Update () {
+	    if(radiusMaterial != null && isEnabled)
+        {
+            if (isEnemy)
+            {
+                color = Color.red;
+            }
+            else
+            {
+                color = Color.green;
+            }
+            radiusMaterial.SetVector("_Center", transform.position);
+            radiusMaterial.SetFloat("_Radius", radius);
+            radiusMaterial.SetColor("_RadiusColor", color);
+        }	
+	}
+}
